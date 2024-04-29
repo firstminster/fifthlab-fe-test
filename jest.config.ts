83bc738ca2b14b6524f56a87161753d894d09727
Chinecherem Harry <chinecherempharry@gmail.com>
2024-04-29 11:00:33 +0100

    feat: setup jest for unit test

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'jsdom',
+    globals: {
+        'ts-jest': {
+            tsconfig: './tsconfig.jest.json',
+        },
+    },
+    setupFilesAfterEnv: ['./src/jest.setup.ts'],
+
+};
